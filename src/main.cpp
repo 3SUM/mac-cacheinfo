b53ac75396ab9973297a9835397ed4c22800a721
Luis Maya Aranda <mayafluis@gmail>
2024-06-13 10:27:53 -0700

    working

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,19 @@
 #include <cstdint>
 #include <print>
 
+size_t get_size() {
+    size_t cacheline = 0;
+    size_t sizeof_cacheline = sizeof(cacheline);
+    sysctlbyname("hw.cachelinesize", &cacheline, &sizeof_cacheline, 0, 0);
+    return cacheline;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 1) {
         std::print("Usage\n\t./cache\n");
         exit(EXIT_FAILURE);
     }
 
+    std::print("Device CPU Cache Line Size: {} bytes\n", get_size());
     exit(EXIT_SUCCESS);
 }
